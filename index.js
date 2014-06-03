bfcb502c2a7cc4e4dcd95ba92f1c33a4bd4853be
karoo <nt10@163.com>
2014-06-04 00:09:55 +0800

    startServer commond

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-module.exports = require('./lib/startserver');
+#! /usr/bin/env node
+
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var path = require('path');
+var root = process.cwd();
+
+http.createServer(function (req, res) {
+  var pathname = url.parse(req.url).pathname;
+  var localPath = root + pathname;
+  path.exists(localPath, function (exists){
+    if(!exists){
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.write("This request URL " + pathname + " was not found on this server.");
+      res.end();
+    } else {
+      fs.readFile(localPath, 'binary', function (err, file){
+        if(err){
+          res.writeHead(500, {'Content-Type': 'text/plain'});
+          res.end();
+        } else {
+          res.writeHead(200, {'Content-Type': 'text/html'});
+          res.write(file, "binary");
+          res.end();
+        }
+      });
+    }
+  });
+
+}).listen(1337, '127.0.0.1');
+
+//module.exports = require('./lib/startserver');
