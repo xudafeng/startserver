762500851a7d0274ad52cd6689e1804ad2f03bdc
karoo <nt10@163.com>
2014-07-19 22:19:28 +0800

    add some test

--- test/startserver.test.js
+++ test/startserver.test.js
@@ -1,7 +1,32 @@
+require("chai").should();
 var StartServer = require('../');
 
 var server = new StartServer();
 
-server.listen(8888, function(){
-  
+describe('StartServer', function () {
+  it('init function must have this members', function () {
+    server.stack.length.should.equal(0);
+  });
+  it('init function must have this members', function () {
+    server._events.start.should.be.a('Function');
+    server._events.next.should.be.a('Function');
+    server._events.end.should.be.a('Function');
+  });
+
 });
+
+describe('StartServer Prototype', function () {
+  it('must has this types', function () {
+    server.listen.should.be.a('Function');
+    server.bundle.should.be.a('Function');
+    server.bind.should.be.a('Function');
+  });
+});
+
+describe('function _listen', function () {
+  it('', function () {
+
+  });
+});
+
+
